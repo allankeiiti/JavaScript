d4dd6d7cac061d3f4102b6fbfdf0599054e054c7
allan.nakakita <allan.nakakita@mlsf.com.br>
2019-08-20 08:26:21 -0300

    Commit Inicial

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function soma(num1, num2){
+    var result = num1 + num2;
+    console.log(result);
+}
+soma(1, 20);
+
+function exibeAlgo(){
+    console.log('ExibeAlgo');
+}
+console.log('teste LOG');
+setTimeout(exibeAlgo, 5000);
